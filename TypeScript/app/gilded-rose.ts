ca4c553ec9afaf3d7ac5672241421fd5042d6282
kamel.romdhani <romdhanikamel30@gmail.com>
2025-08-19 22:13:02 +0200

    put else if in 1 line and item.quality - item.quality is 0

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -59,16 +59,18 @@ export class GildedRose {
         if (item.quality < 50) {
           item.quality = item.quality + 1
         }
-      } else {
-        if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-          item.quality = item.quality - item.quality
-          
-        } else {
+      } 
+      else if (item.name === 'Backstage passes to a TAFKAL80ETC concert'){
+        item.quality = 0;
+      }
+      else {
           if (item.quality > 0) {
             item.quality = item.quality - 1
           }
-        }
       }
+      
     }
   }
 }
+
+
