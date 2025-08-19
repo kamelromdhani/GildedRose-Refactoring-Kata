2b4e05e5109e9bddd521921a47b9ce22fd9083ea
kamel.romdhani <romdhanikamel30@gmail.com>
2025-08-19 22:01:51 +0200

    replace != with === in Backstage passes

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -59,16 +59,14 @@ export class GildedRose {
         if (item.quality < 50) {
           item.quality = item.quality + 1
         }
-        
       } else {
-        if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.quality > 0) {
-
-            item.quality = item.quality - 1
-
-          }
-        } else {
+        if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
           item.quality = item.quality - item.quality
+          
+        } else {
+          if (item.quality > 0) {
+            item.quality = item.quality - 1
+          }
         }
       }
     }
