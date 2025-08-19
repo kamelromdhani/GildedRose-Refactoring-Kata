993907c1c4910a9a356547ed39360ed2dc46c4bb
kamel.romdhani <romdhanikamel30@gmail.com>
2025-08-19 21:59:10 +0200

    replace != with === in the item.sellIn < 0 condition and switch if and else blocks for better readability

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -48,16 +48,19 @@ export class GildedRose {
       }
     } else {
       if (item.quality > 0) {
-
         item.quality = item.quality - 1
-
       }
     }
 
     item.sellIn = item.sellIn - 1;
 
     if (item.sellIn < 0) {
-      if (item.name != 'Aged Brie') {
+      if (item.name === 'Aged Brie') {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+        }
+        
+      } else {
         if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (item.quality > 0) {
 
@@ -66,10 +69,6 @@ export class GildedRose {
           }
         } else {
           item.quality = item.quality - item.quality
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
         }
       }
     }
