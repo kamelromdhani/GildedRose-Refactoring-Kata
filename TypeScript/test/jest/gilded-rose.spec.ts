f66dd5f2ab436ac248ffec30578d5a82ef53f7b9
kamel.romdhani <romdhanikamel30@gmail.com>
2025-08-19 21:36:35 +0200

    add test cases for Backstage passes

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -77,10 +77,61 @@ describe('Gilded Rose', () => {
     });
   });
 
+    describe('Backstage Passes', () => {
+    it('should increase quality by 1 when sellIn > 10', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0]).toEqual({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 14,
+        quality: 21,
+      });
+    });
+
+    it('should increase quality by 2 when 6 <= sellIn <= 10', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20)]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0]).toEqual({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 9,
+        quality: 22,
+      });
+    });
+
+    it('should increase quality by 3 when 0 < sellIn <= 5', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20)]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0]).toEqual({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 4,
+        quality: 23,
+      });
+    });
+
+    it('should drop quality to 0 when sellIn <= 0', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20)]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0]).toEqual({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: -1,
+        quality: 0,
+      });
+    });
+
+    it('should not increase quality above 50', () => {
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49)]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0]).toEqual({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 4,
+        quality: 50,
+      });
+    });
+  });
+
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
 });
-
